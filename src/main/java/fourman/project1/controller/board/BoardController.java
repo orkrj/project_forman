f2f97b80208c6c03149275d999741d9be3234ed8
Jeu <jeu0901@daum.net>
2024-11-11 22:36:03 +0900

    feat: Board Liveservice 기능 구현 수정

--- src/main/java/fourman/project1/controller/board/BoardController.java
+++ src/main/java/fourman/project1/controller/board/BoardController.java
@@ -22,39 +22,21 @@ public class BoardController {
    private final BoardService boardService;
    private final BoardMapper boardMapper;
 
+
    @GetMapping
-    public String findBoards(Model model) {
-       List<Board> boards = boardService.findBoards();
-       model.addAttribute("boards", boards);
-       return "";
-   }
+   public String findBoardById(Model model) {
+      Board findBoard = boardService.findBoardById((long) 1);
 
-   @GetMapping("/{boardId}")
-   public String findBoardById(@PathVariable Long boardId, Model model) {
-      Board findBoard = boardService.findBoardById(boardId);
+      List<Integer> randomIdx = boardService.random();
 
-      // findBoard가 null인 경우 처리
-      if (findBoard == null) {
-         model.addAttribute("error", "해당 ID의 Board를 찾을 수 없습니다.");
-         return "board";  // board 화면으로 돌아가도록 처리
-      }
-
-      // Board 안에 있는 Post들을 랜덤으로 3개 선택
-      List<Post> posts = findBoard.getPosts();
-
-      if (posts.size() >= 3) {
-         Collections.shuffle(posts); // 리스트 무작위 섞기
-         posts = posts.subList(0, 3); // 임의의 3개 선택
-      } else {
-         // 부족한 post 수만큼 null을 추가하여 총 3개 채우기
-         while (posts.size() < 3) {
-            posts.add(null); // null 추가
-         }
-      }
-
+      Post first = findBoard.getPosts().get(randomIdx.get(0));
+      Post second = findBoard.getPosts().get(randomIdx.get(1));
+      Post third = findBoard.getPosts().get(randomIdx.get(2));
 
       model.addAttribute("board", findBoard);
-      model.addAttribute("randomPosts", posts); // 랜덤으로 선택된 post들
+      model.addAttribute("first", first);
+      model.addAttribute("second", second);
+      model.addAttribute("third", third);
 
       return "board";
    }
