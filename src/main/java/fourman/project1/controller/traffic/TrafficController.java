0152fba7a568a22f35235263adcfc87391fd4adb
Zun <k5orkrj@gmail.com>
2024-11-11 14:34:44 +0900

    feat: convey trafficId to controller before async processing

--- src/main/java/fourman/project1/controller/traffic/TrafficController.java
+++ src/main/java/fourman/project1/controller/traffic/TrafficController.java
@@ -52,7 +52,6 @@ public class TrafficController {
                 trafficMapper.trafficRequestDtoToTrafficResponseDto(trafficRequestDto)
         );
 
-        return "redirect:/traffics";
+        return "redirect:/traffics/" + trafficId.join();
     }
 }
-d
