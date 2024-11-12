77071f04c46883b26b6dae90c33695d81498bdba
박민서 <qkrals2475@naver.com>
2024-11-12 13:15:02 +0900

    fead: 로그인된 사용자의 userId 가져오는 로직 추가

--- src/main/java/fourman/project1/domain/user/CustomUserDetails.java
+++ src/main/java/fourman/project1/domain/user/CustomUserDetails.java
@@ -52,4 +52,8 @@ public class CustomUserDetails implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public Long getUserId(){
+        return user.getUserId();
+    }
 }
