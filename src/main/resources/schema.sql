2b4a9639e2106f2d744215c6caacda3de8249f39
Zun <k5orkrj@gmail.com>
2024-11-13 10:17:41 +0900

    fix: resolve merge conflicts with feature-dev-post-delete branch

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS users (
 
         user_id BIGINT AUTO_INCREMENT PRIMARY KEY,
         username VARCHAR(15) NOT NULL,
-        password VARCHAR(15) NOT NULL,
+        password VARCHAR(100) NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
         deleted_at TIMESTAMP DEFAULT NULL
@@ -40,9 +40,9 @@ DROP TABLE IF EXISTS traffics;
 CREATE TABLE traffics (
 
         traffic_id    BIGINT AUTO_INCREMENT PRIMARY KEY,
-        url        VARCHAR(200),
+        url        VARCHAR(100),
         vus        BIGINT       NOT NULL,
-        duration   VARCHAR(20)  NOT NULL,
+        duration   VARCHAR(10)  NOT NULL,
         total_req  BIGINT,
         avg_req    BIGINT,
         user_id    BIGINT,
